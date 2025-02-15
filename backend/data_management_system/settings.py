da2a5cb2be7d84a1493d9db60e77bdfbf56a9516
Rami Atallah <ramiatallah63@gmail.com>
2025-02-15 23:12:02 +0200

    add TEMPLATES and STATICFILES_DIRS to settings.py

--- backend/data_management_system/settings.py
+++ backend/data_management_system/settings.py
@@ -49,6 +49,29 @@ CORS_ALLOW_HEADERS = ["*"]
 
 ROOT_URLCONF = "data_management_system.urls"
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "assignment_data_management" / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "assignment_data_management" / "static",
+]
+
+WSGI_APPLICATION = "assignment_data_management.wsgi.application"
+
 # Database settings
 DATABASES = {
     "default": {
@@ -60,6 +83,47 @@ DATABASES = {
         "PORT": env("DB_PORT"),
     }
 }
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = "static/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
