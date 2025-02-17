f8ba14e54190af761ddc7f4aa987d778d5317cc8
Rami Atallah <ramiatallah63@gmail.com>
2025-02-17 10:42:24 +0200

    get email dynamically from settings.py

--- backend/schema_manager/utils.py
+++ backend/schema_manager/utils.py
@@ -1,11 +1,12 @@
 from django.core.mail import send_mail
+from django.conf import settings
 
 
 def send_import_confirmation(title, message, email):
     send_mail(
         title,
         message,
-        "ramiatallah722@gmail.com",
+        settings.EMAIL_HOST_USER,
         [email],
         fail_silently=True,
     )
